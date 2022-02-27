c797ac93928b4ba53d1e78439513b28c8e1fc9b7
Ryan <ryan.clark4@bestbuy.com>
2022-02-26 18:01:57 -0800

    Include and IncludeMultiple - includes member data sets to models

---
+++ IncludeExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RyansEye.Api.Extensions
+{
+    public static class IncludeExtension
+    {
+        public static IQueryable<TEntity> Include<TEntity>(this IDbSet<TEntity> dbSet,
+                                                params Expression<Func<TEntity, object>>[] includes)
+                                                where TEntity : class
+        {
+            IQueryable<TEntity> query = null;
+            foreach (var include in includes)
+            {
+                query = dbSet.Include(include);
+            }
+
+            return query == null ? dbSet : query;
+        }
+
+        public static IQueryable<T> IncludeMultiple<T>(this IQueryable<T> query,
+            params Expression<Func<T, object>>[] includes) where T : class
+        {
+            if (includes != null)
+            {
+                query = includes.Aggregate(query,
+                          (current, include) => current.Include(include));
+            }
+
+            return query;
+        }
+    }
+}
