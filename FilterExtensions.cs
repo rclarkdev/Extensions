1d820d2f9c610cf50d04efdb62bca27c3e458b73
Ryan <ryan.clark4@bestbuy.com>
2022-02-26 17:58:13 -0800

    FilterById - Allows for the filtering a list of generic objects by Id

---
+++ FilterExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+
+namespace RyansEye.Api.Extensions
+{
+    public static class FilterExtensions
+    {
+        public static List<T> FilterById<T>(this List<T> collection, long id)
+        {
+            var filteredCollection = new List<T>();
+            foreach (var item in collection)
+            {
+                var propertyInfo = item.GetType().GetProperty("Id", BindingFlags.Public | BindingFlags.Instance);
+                var propertyValue = propertyInfo.GetValue(item, null);
+                if ((long)propertyValue == id) filteredCollection.Add(item);
+            }
+            return filteredCollection;
+        }
+    }
+}
